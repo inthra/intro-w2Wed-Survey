ec2f10fa79bfe20aed1307693ce8b8d8aaf52bfa
Inthrayuth Mahaphol and Ned Faulhaber <ned@>
2016-03-23 15:49:24 -0700

    try to have submit button equal to a results form

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,21 @@
-$(do)
+$(document).ready(function() {
+  $("#blanks form").submit(function(event) {
+    var nameInput = $("input#name").val();
+    var emailInput = $("input#email").val();
+    var bornInput= $("input#born").val();
+    var genreInput = $("input.genre").val();
+    var composerInput = $("input.composer").val();
+    var instrumentInput = $("input.instrument").val();
+
+    $(".name").text(nameInput);
+    $(".email").text(emailInput);
+    $(".born").text(bornInput);
+    $(".genre").text(genreInput);
+    $(".composer").text(composerInput);
+    $(".instrument").text(instrumentInput);
+
+    $("#result").show();
+
+    event.preventDefault();
+  });
+});
