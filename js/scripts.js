4bc2404cabb3a716f805d9d38dc84f7e671f8b01
IM <zign.int@gmail.com>
2016-03-26 17:49:36 -0700

    fixed the Submit Button and Radio script

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,10 @@ $(document).ready(function() {
   $("#blanks form").submit(function(event) {
     var nameInput = $("input#name").val();
     var emailInput = $("input#email").val();
-    var bornInput= $("input#born").val();
-    var genreInput = $("input.genre").val();
-    var composerInput = $("input.composer").val();
-    var instrumentInput = $("input.instrument").val();
+    var bornInput = $("input#born").val();
+    var genreInput = $("input:radio[name=genre]:checked").val();
+    var composerInput = $("input:radio[name=composer]:checked").val();
+    var instrumentInput = $("input:radio[name=instrument]:checked").val();
 
     $(".name").text(nameInput);
     $(".email").text(emailInput);
