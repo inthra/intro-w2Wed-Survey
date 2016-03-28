23f6e1d6ffc141129a14d20c51f7f99a54f75ba7
IM <zign.int@gmail.com>
2016-03-27 18:40:49 -0700

    add Favorite Color Form Input

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,16 @@ $(document).ready(function() {
     var nameInput = $("input#name").val();
     var emailInput = $("input#email").val();
     var bornInput = $("input#born").val();
+    var favoriteColor = $("input#color").val();
     var genreInput = $("input:radio[name=genre]:checked").val();
     var composerInput = $("input:radio[name=composer]:checked").val();
     var instrumentInput = $("input:radio[name=instrument]:checked").val();
 
+
     $(".name").text(nameInput);
     $(".email").text(emailInput);
     $(".born").text(bornInput);
+    $(".color").text(favoriteColor);
     $(".genre").text(genreInput);
     $(".composer").text(composerInput);
     $(".instrument").text(instrumentInput);
